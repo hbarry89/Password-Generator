82e026376108f733ef2dfe7acc368132f570f05b
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-01-27 13:25:31 -0500

    organize code as directed by TA in class

--- script.js
+++ script.js
@@ -41,18 +41,20 @@
   
 //the password is displayed to the screen
 
-
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var lowerCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '>', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']; //characters are taken from OWASP link in instructions.
+  // Added backslash to ' and \ characters to disable them from functioning as part of the code
+  var result = [];  
 
 // // Assignment Code
 var generateBtn = document.querySelector("#generate"); //starter code
 
-// // Write password to the #password input
-function writePassword() { //starter code
-  var password = generatePassword(); //starter code
-  var passwordText = document.querySelector("#password"); //starter code
-  var passwordLength = prompt("How many characters would you like your password to contain?");
-  passwordText.value = password; //starter code
+function getOptions() {
 
+  var passwordLength = prompt("How many characters would you like your password to contain?");
+  
   if (passwordLength < 8 || passwordLength > 128) { //Making sure password length meets critera in instructions provided by challenge readme file: "at least 8 characters and no more than 128 characters"
     alert('Password length must be at least 8 characters and no more than 128 characters');
   }
@@ -61,7 +63,7 @@ function writePassword() { //starter code
   var hasLower = confirm("Click OK to confirm including Lower characters.");
   var hasUpper = confirm("Click OK to confirm including Upper characters.");
   var hasSpecial = confirm("Click OK to confirm including Special characters.");
-
+  var possibleChars = [];
   if (hasNumeric === false && hasLower === false && hasUpper === false && hasSpecial === false) {
     alert('Must select at least one character type');
   }
@@ -73,31 +75,52 @@ function writePassword() { //starter code
       hasUpper: hasUpper,
       hasSpecial: hasSpecial,
   }
+
   return userPassword;
+}
 
-   //create another function to generate the password
-  function generatePassword() {
-    
-  }
-  //var userOptions = getOptions();
-  var userOptions = getOptions();
-
-  function getOptions()
-
-  //we have an array to store each criteria
+function generatePassword(){
+  var options = getOptions();
   var possibleChars = [];
-  var result = [];
-  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  var lowerCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '>', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']; //characters are taken from OWASP link in instructions.
-  // Added backslash to ' and \ characters to disable them from functioning as part of the code
-  
-  if (userOptions.hasNumeric === true) {
+  var finalPass = [];
+  if (options.hasNumeric === true) {
     possibleChars = possibleChars.concat(numericCharacters);
   }
-
+  if (options.hasUpper === true) {
+    possibleChars = possibleChars.concat(upperCharacters);
+  }
+  if (options.hasLower === true) {
+    possibleChars = possibleChars.concat(lowerCharacters);
+  }
+  if (options.hasSpecial === true) {
+    possibleChars = possibleChars.concat(specialCharacters);
+  }
+  //loop over password length from options, select random elements to add 
+  for(var i = 0; i < options.length; i++) {
+    //grab random element out of possibleChars
+    //push random element into final pass
+  }
+  console.log(possibleChars);
 }
+
+// // Write password to the #password input
+function writePassword() { //starter code
+  var password = generatePassword(); //starter code
+  var passwordText = document.querySelector("#password"); //starter code
+  passwordText.value = password; //starter code
+  
+   //create another function to generate the password
+  //function generatePassword() {
+
+  //we have an array to store each criteria
+  //var possibleChars = [];
+  //var userOptions = getOptions() 
+
+  }
+
+//}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); //starter code
