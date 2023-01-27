3698cf5b4f7710090890adf55f168cdda7f567a7
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-01-27 00:03:41 -0500

    add special characters array string

--- script.js
+++ script.js
@@ -88,7 +88,8 @@ function writePassword() { //starter code
   var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   var lowerCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var specialCharacters = []
+  var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '>', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+  // Added backslash to ' and \ characters to disable them from functioning as part of the code
   
   if (userOptions.hasNumeric === true) {
     possibleChars = possibleChars.concat(numericCharacters);
